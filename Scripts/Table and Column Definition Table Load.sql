c541843e2501ce004e2f2ed3d12fcfa728124fd7
esgTom <esgTom@entara.com>
2017-08-30 07:19:50 -0500

    Modifications to Table and Column definitions

--- Scripts/Table and Column Definition Table Load.sql
+++ Scripts/Table and Column Definition Table Load.sql
@@ -13,18 +13,18 @@ SET @pkValue = @@IDENTITY;
 
 INSERT INTO Column_Definition(
 			Table_Id, Column_Name, Column_Description, Column_SQL_Data_Type, Column_API_Data_Type, Column_Client_Data_Type, 
-			Column_Length, Column_Decimal_Places, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
+			Column_Length, Column_Decimal_Places, Column_Nullable, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
 			Created_By, Created_Date, Modified_By,  Modified_Date)
      VALUES( @pkValue, 'Workout Identity','Workout Identity','int','int','Number', 
-			NULL, NULL, 'Workout_Id','WorkoutId','WorkoutId','DB Load', 
+			NULL, NULL, 0, 'Workout_Id','WorkoutId','WorkoutId','DB Load', 
 			GETDATE(), 'DB Load', GETDATE() )
 
 INSERT INTO Column_Definition(
 			Table_Id, Column_Name, Column_Description, Column_SQL_Data_Type, Column_API_Data_Type, Column_Client_Data_Type, 
-			Column_Length, Column_Decimal_Places, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
+			Column_Length, Column_Decimal_Places, Column_Nullable, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
 			Created_By, Created_Date, Modified_By,  Modified_Date)
      VALUES( @pkValue, 'Workout Type','Workout Type - bike, weights, swim, etc','varchar(50)','String','String', 
-			50, NULL, 'Workout_Type','WorkoutType','WorkoutType','DB Load', 
+			50, NULL, 1, 'Workout_Type','WorkoutType','WorkoutType','DB Load', 
 			GETDATE(), 'DB Load', GETDATE() )
 
 		
@@ -34,18 +34,18 @@ SET @pkValue = @@IDENTITY;
 
 INSERT INTO Column_Definition(
 			Table_Id, Column_Name, Column_Description, Column_SQL_Data_Type, Column_API_Data_Type, Column_Client_Data_Type, 
-			Column_Length, Column_Decimal_Places, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
+			Column_Length, Column_Decimal_Places, Column_Nullable, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
 			Created_By, Created_Date, Modified_By,  Modified_Date)
      VALUES( @pkValue, 'Schedule Identity','Schedule Identity','int','int','Number', 
-			NULL, NULL, 'Schedule_Id','ScheduleId','ScheduleId','DB Load', 
+			NULL, NULL, 0, 'Schedule_Id','ScheduleId','ScheduleId','DB Load', 
 			GETDATE(), 'DB Load', GETDATE() )
 
 INSERT INTO Column_Definition(
 			Table_Id, Column_Name, Column_Description, Column_SQL_Data_Type, Column_API_Data_Type, Column_Client_Data_Type, 
-			Column_Length, Column_Decimal_Places, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
+			Column_Length, Column_Decimal_Places, Column_Nullable, Column_SQL_Name, Column_API_Name, Column_Client_Name, 
 			Created_By, Created_Date, Modified_By,  Modified_Date)
      VALUES( @pkValue, 'Schedule Category','What is being scheduled - workouts, nutrition','varchar(50)','String','String', 
-			50, NULL, 'ScheduleCategory','ScheduleCategory','ScheduleCategory','DB Load', 
+			50, NULL, 1, 'ScheduleCategory','ScheduleCategory','ScheduleCategory','DB Load', 
 			GETDATE(), 'DB Load', GETDATE() )
 
 			
